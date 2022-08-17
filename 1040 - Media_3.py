0ddedd8117f3a945fffb8a26447b876fe31a2e85
Sérgio Lima <sergiodelima22@gmail.com>
2022-08-16 22:33:20 -0300

    versao-02-descobrindo-o-que-o-enunciado-quer-dizer

---
+++ 1040 - Media_3.py
@@ -0,0 +1,15 @@
+#Leia quatro números (N1, N2, N3, N4), cada um deles com uma casa decimal, correspondente às quatro notas de um aluno. Calcule a média com pesos 2, 3, 4 e 1, respectivamente, para cada uma destas notas e mostre esta média acompanhada pela mensagem "Media: ". Se esta média for maior ou igual a 7.0, imprima a mensagem "Aluno aprovado.". Se a média calculada for inferior a 5.0, imprima a mensagem "Aluno reprovado.". Se a média calculada for um valor entre 5.0 e 6.9, inclusive estas, o programa deve imprimir a mensagem "Aluno em exame.".
+
+#No caso do aluno estar em exame, leia um valor correspondente à nota do exame obtida pelo aluno. Imprima então a mensagem "Nota do exame: " acompanhada pela nota digitada. Recalcule a média (some a pontuação do exame com a média anteriormente calculada e divida por 2). e imprima a mensagem "Aluno aprovado." (caso a média final seja 5.0 ou mais ) ou "Aluno reprovado.", (caso a média tenha ficado 4.9 ou menos). Para estes dois casos (aprovado ou reprovado após ter pego exame) apresente na última linha uma mensagem "Media final: " seguido da média final para esse aluno.
+
+# PASS A PASSO:
+# 1 - Criar variáveis de entrada;
+# 2 - Calcule a média com base nas notas dadas pelo enunciado do problema;
+
+lista_1 = str(input())
+lista_2 = lista_1.split()
+nota1 = float(lista_2[0])
+nota2 = float(lista_2[1])
+nota3 = float(lista_2[2])
+nota4 = float(lista_2[3])
+print(nota1,nota2,nota3,nota4)
