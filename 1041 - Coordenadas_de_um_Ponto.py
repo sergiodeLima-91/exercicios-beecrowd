2175e47df1d83959174f25ba78a18328ce6d6794
Sérgio Lima <sergiodelima22@gmail.com>
2022-09-01 11:29:40 -0300

    versao-01-construcao-da-logica

---
+++ 1041 - Coordenadas_de_um_Ponto.py
@@ -0,0 +1,13 @@
+# 1 - Leia 2 valores com uma casa decimal (x e y);
+# 2 - Estes valores devem representar as coordenadas de um ponto em um plano.
+# 3 - Determine qual o quadrante ao qual pertence o ponto, ou se está sobre um dos eixos cartesianos ou na origem (x = y = 0).
+
+# Criando duas variáveis de entrada para ler os dois valores.
+
+v1 = float(input())
+v2 = float(input())
+
+# Como representar os quadrantes dos eixos x e y? Ambos são compostos por números de natureza positiva e negativa. Então basta colocá-los nesta ordem em cada eixod dentro de uma variável.
+
+x = []
+y = []
