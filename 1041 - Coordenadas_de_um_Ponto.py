a0e143d88c3eb21006009816249fafec7cb44e1a
Sérgio Lima <sergiodelima22@gmail.com>
2022-09-02 15:47:04 -0300

    versao-02-codigo-completo-com-partes-de-Guanabara

--- 1041 - Coordenadas_de_um_Ponto.py
+++ 1041 - Coordenadas_de_um_Ponto.py
@@ -39,3 +39,30 @@ elif y == 0 and x < 0:
     print('Eixo Y')
 elif x == 0 and y == 0:
     print('Origem')
+
+# Outro código retirado de http://muitomaiscodigoss.blogspot.com/2018/09/uri-problema-1041-coordenadas-de-um.html para verificar porque eu estou errando.
+
+'''p = input().split()
+x, y = p
+
+x = float(x)
+y = float(y)
+
+if x == 0:
+    if y == 0:
+        print('Origem')
+    if y != 0:
+        print('Eixo Y')
+if y == 0:
+    if x != 0:
+        print('Eixo X')
+if x > 0:
+    if y > 0:
+        print('Q1')
+    if y < 0:
+        print('Q4')
+if x < 0:
+    if y > 0:
+        print('Q2')
+    if y < 0:
+        print('Q3')'''
