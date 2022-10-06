f899d29d63c8b0381108757c1c6502f7c679ec38
Sérgio Lima <sergiodelima22@gmail.com>
2022-10-05 21:48:23 -0300

    solucao-encontrada-e-outras-tentivas-de-resolucao

--- 1041 - Coordenadas_de_um_Ponto.py
+++ 1041 - Coordenadas_de_um_Ponto.py
@@ -4,11 +4,6 @@
 
 # Criando duas variáveis de entrada para ler os dois valores.
 
-lista1 = str(input())
-lista2 = lista1.split()
-x = float(lista2[0])
-y = float(lista2[1])
-
 # Como representar os quadrantes dos eixos x e y? Ambos são compostos por números de natureza positiva e negativa. Então basta colocá-los nesta ordem em cada eixo dentro de uma variável. Necessário entender o seguinte, seguindo um raciocínio lógico:
 
 # Quadrante 1: X - POSITIVO e Y - POSITIVO
@@ -18,29 +13,36 @@ y = float(lista2[1])
 
 # Portanto, para determinar em qual quadrante as coordenadas estarão:
 
-if x > 0 and y > 0:
-    print('Q1')
-elif x < 0 and y > 0:
-    print('Q2')
-elif x < 0 and y < 0:
-    print('Q3')
-elif x > 0 and y < 0:
-    print('Q4')
+l = input().split()
+x, y = l
 
-# Caso as coordenadas mostrem que um dos pontos está sobre um dos eixos:
+x = float(x)
+y = float(y)
 
-if x == 0 and y > 0:
-    print('Eixo X')
-elif x == 0 and y < 0:
-    print('Eixo X')
-elif y == 0 and x > 0:
-    print('Eixo Y')
-elif y == 0 and x < 0:
-    print('Eixo Y')
-elif x == 0 and y == 0:
-    print('Origem')
+# PARTE PARA DEFINIR QUAL É O QUADRANTE:
+if y > 0:
+    if x > 0:
+        print('Q1')
+    if x < 0:
+        print('Q2')
+if y < 0:
+    if x < 0:
+        print('Q3')
+    if x > 0:
+        print('Q4')
 
-# Outro código retirado de http://muitomaiscodigoss.blogspot.com/2018/09/uri-problema-1041-coordenadas-de-um.html para verificar porque eu estou errando.
+# PARTE PARA DEFINIR QUAL É O PONTO CENTRAL:
+if y == 0:
+    if x == 0:
+        print('Origem')
+    if x != 0:
+        print('Eixo X')
+if x == 0:
+    if y != 0:
+        print('Eixo Y')
+
+# Outro código retirado de http://muitomaiscodigoss.blogspot.com/2018/09/uri-problema-1041-coordenadas-de-um.html para
+# verificar porque eu estou errando.
 
 '''p = input().split()
 x, y = p
